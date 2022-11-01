956009f457567f695078cd7cb91a2f334eb24d97
James Zhou <james7.ca@gmail.com>
2022-10-31 22:00:06 -0400

    POJO long practice

---
+++ problems/13-string-converter.js
@@ -0,0 +1,33 @@
+/***********************************************************************
+Write a function `stringConverter(string)` that will intake a
+string as an argument and returns an object representing the count of each
+character in the string. **Hint:** don't forget you can check if a key is present
+in an object by using `obj[key] === undefined`.
+
+Examples:
+
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
+***********************************************************************/
+
+function stringConverter(string) {
+  // Your code here
+  let obj = {};
+  //obj[string[0]] = 1;
+  for (let i = 0; i < string.length; i++) {
+    //console.log(string[i]);
+    if (obj[string[i]] === undefined) obj[string[i]] = 1;
+    else obj[string[i]]++;
+
+  }
+
+  return obj;
+}
+
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = stringConverter;
